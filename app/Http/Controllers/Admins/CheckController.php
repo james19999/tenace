9630decdf3a1e7ee0aa250c9ac70f4717cb12408
james <komlanahiakpor23@gmail.com>
2024-01-24 21:19:32 +0100

    HI

--- app/Http/Controllers/Admins/CheckController.php
+++ app/Http/Controllers/Admins/CheckController.php
@@ -101,7 +101,7 @@ class CheckController extends Controller
                           $orderItem->order_id = $order->id;
                           $orderItem->price = $item->price;
                           $orderItem->quantity = $item->qty;
-                          $orderItem->high_price = $item->high_price ;
+                          $item->high_price ?$orderItem->high_price = $item->high_price : $orderItem->high_price=0 ;
                           $orderItem->save();
                       }
 
