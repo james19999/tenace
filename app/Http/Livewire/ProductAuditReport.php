93ac9839d1e47863d7a84355be4c8f824a12696f
jameslalachante <komlanjameswdev.lalachante@gmail.com>
2026-06-20 12:46:27 +0200

    n

--- app/Http/Livewire/ProductAuditReport.php
+++ app/Http/Livewire/ProductAuditReport.php
@@ -18,7 +18,7 @@ class ProductAuditReport extends Component
     public $selectedMonth;
     public $selectedYear;
 
-    public $totaldif = 0;
+    public $totaldif;
     public $total = 0;
     public function mount()
     {
@@ -51,77 +51,77 @@ class ProductAuditReport extends Component
             ->join('orders', 'orders.id', '=', 'order_items.order_id')
             ->where('orders.status', 'delivered');
 
-            $totalQuery = Order::query()
-           ->where('status', 'delivered');
+        $totalQuery = Order::query()
+            ->where('status', 'delivered');
 
-switch ($this->filterType) {
+        switch ($this->filterType) {
 
-    case 'day':
+            case 'day':
 
-        $query->whereDate(
-            'orders.created_at',
-            $this->selectedDay
-        );
+                $query->whereDate(
+                    'orders.created_at',
+                    $this->selectedDay
+                );
 
-        $totalQuery->whereDate(
-            'created_at',
-            $this->selectedDay
-        );
+                $totalQuery->whereDate(
+                    'created_at',
+                    $this->selectedDay
+                );
 
-        break;
+                break;
 
-    case 'week':
+            case 'week':
 
-        if ($this->selectedWeek) {
+                if ($this->selectedWeek) {
 
-            [$year, $week] = explode('-W', $this->selectedWeek);
+                    [$year, $week] = explode('-W', $this->selectedWeek);
 
-            $yearWeek = $year . str_pad($week, 2, '0', STR_PAD_LEFT);
+                    $yearWeek = $year . str_pad($week, 2, '0', STR_PAD_LEFT);
 
-            $query->whereRaw(
-                'YEARWEEK(orders.created_at,1) = ?',
-                [$yearWeek]
-            );
+                    $query->whereRaw(
+                        'YEARWEEK(orders.created_at,1) = ?',
+                        [$yearWeek]
+                    );
 
-            $totalQuery->whereRaw(
-                'YEARWEEK(created_at,1) = ?',
-                [$yearWeek]
-            );
+                    $totalQuery->whereRaw(
+                        'YEARWEEK(created_at,1) = ?',
+                        [$yearWeek]
+                    );
+                }
+
+                break;
+
+            case 'month':
+
+                if ($this->selectedMonth) {
+
+                    [$year, $month] = explode('-', $this->selectedMonth);
+
+                    $query->whereYear('orders.created_at', $year)
+                        ->whereMonth('orders.created_at', $month);
+
+                    $totalQuery->whereYear('created_at', $year)
+                        ->whereMonth('created_at', $month);
+                }
+
+                break;
+
+            case 'year':
+
+                $query->whereYear(
+                    'orders.created_at',
+                    $this->selectedYear
+                );
+
+                $totalQuery->whereYear(
+                    'created_at',
+                    $this->selectedYear
+                );
+
+                break;
         }
 
-        break;
-
-    case 'month':
-
-        if ($this->selectedMonth) {
-
-            [$year, $month] = explode('-', $this->selectedMonth);
-
-            $query->whereYear('orders.created_at', $year)
-                  ->whereMonth('orders.created_at', $month);
-
-            $totalQuery->whereYear('created_at', $year)
-                     ->whereMonth('created_at', $month);
-        }
-
-        break;
-
-    case 'year':
-
-        $query->whereYear(
-            'orders.created_at',
-            $this->selectedYear
-        );
-
-        $totalQuery->whereYear(
-            'created_at',
-            $this->selectedYear
-        );
-
-        break;
-}
-
-$this->total = $totalQuery->sum('total');
+        $this->total = $totalQuery->sum('total');
         $this->reports = $query
             ->select(
                 'products.id',
@@ -153,16 +153,16 @@ $this->total = $totalQuery->sum('total');
     }
 
     public function getNetAmountProperty()
-{
-    $this->totaldif= $this->totalAmount -$this->total;
-    return $this->totalAmount-$this->totaldif;
-}
+    {
+        $this->totaldif = $this->totalAmount - $this->total;
+        return $this->totalAmount - $this->totaldif;
+    }
     public function render()
     {
         $totalYesterday = Order::where('status', 'delivered')
-    ->whereDate('created_at', Carbon::yesterday())
-    ->sum('total');
-        return view('livewire.product-audit-report',['totalYesterday'=>$totalYesterday])
+            ->whereDate('created_at', Carbon::yesterday())
+            ->sum('total');
+        return view('livewire.product-audit-report', ['totalYesterday' => $totalYesterday])
             ->extends('layouts.admin')
             ->section('content');
     }
